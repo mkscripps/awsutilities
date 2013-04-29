587c2d24aaaca98ec4a885ea65e7b6773b9e96db
mkscripps <mark.kelly@scrippsnetworks.com>
2013-04-29 10:23:22 -0400

    Adding storage class checking and file size checking before changing to reduced redundancy

--- SetBucketReducedRedundancy.py
+++ SetBucketReducedRedundancy.py
@@ -13,9 +13,9 @@ __author__ = 'mkelly'
 from boto.s3.connection import S3Connection
 
 conn = S3Connection()
-bucket = conn.get_bucket('sn-amazon')
+bucket = conn.get_bucket('sni-transcode')
 for key in bucket.list():
-    #due to 5gb limit need to check object size
-    if key.size < 5*1024*1024*1024:
+    #due to 5gb limit need to check object size and make sure it is already "STANDARD" storage class
+    if key.size < 5*1024*1024*1024 and key.storage_class == 'STANDARD':
         key.change_storage_class('REDUCED_REDUNDANCY')
-        print "%s has been changed to Reduced redundancy storage" % key.name.encode('utf-8')
+    print "%40s %19s %s" % (key.name.encode('utf-8'),key.storage_class,key.size)
